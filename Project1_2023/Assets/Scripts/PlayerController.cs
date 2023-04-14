51df84a2f3b19d19f37663c9e64ee999908e8f26
WinnieThePio <81216801+JoshuaMiller-Pio@users.noreply.github.com>
2023-04-14 16:32:57 +0200

    added some animations, player movement
    added roll left
    added roll right
    added jump
    added sprint
    added temp track

--- Project1_2023/Assets/Scripts/PlayerController.cs
+++ Project1_2023/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,120 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using System.Timers;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    private GameObject player = new GameObject();
+    private Rigidbody _rigComp = new Rigidbody();
+    private Animator _aniComp = new Animator();
+
+    
     void Start()
     {
-        
+        _rigComp = GetComponent<Rigidbody>();
+        _aniComp = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //constant movement on the z-axis
+        _rigComp.velocity = new UnityEngine.Vector3(_rigComp.velocity.x, _rigComp.velocity.y, 10);
+        move();
+
+
     }
+
+    public void move()
+    {
+        UnityEngine.Vector3 playerPos = transform.position;
+
+        //allows the character to jump
+        if (Input.GetKeyDown("space"))
+        {
+           
+         
+            _rigComp.velocity = new UnityEngine.Vector3(_rigComp.velocity.x, 5, _rigComp.velocity.z);
+            Jumpstart();
+
+        }
+
+        float middle, left, right;
+        middle = .81f;
+        right = 6.58f;
+        left = -5.17f;
+        //will switch the character between the axis
+        if (Input.GetKeyDown("a") || Input.GetKeyDown("left"))
+        {
+            rollleftStart();
+            Debug.Log("left");
+            if (playerPos.x == middle)
+            {
+                playerPos.x = left;
+
+            }
+            else if (playerPos.x == right)
+            {
+                playerPos.x = middle;
+            }
+
+        }
+
+        if (Input.GetKeyDown("d") || Input.GetKeyDown("right"))
+        {
+            rollRightStart();
+            Debug.Log("right");
+            if (playerPos.x == middle)
+            {
+                playerPos.x = right; 
+
+            }
+            else if(playerPos.x == left)
+            {
+                playerPos.x = middle;
+            }
+
+        }
+
+    } 
+
+    public void Jumpstart()
+    {
+        _aniComp.SetBool("isJumping", true);
+
+    }
+    public void JumpEnd()
+    {
+        _aniComp.SetBool("isJumping", false);
+
+    }
+    public void rollleftStart()
+    {
+        _aniComp.SetBool("rollLeft", true);
+
+    }
+    public void rollleftEnd()
+    {
+        _aniComp.SetBool("rollLeft", false);
+
+    }
+    public void rollRightStart()
+    {
+        _aniComp.SetBool("rollRight", true);
+
+    }
+    public void rollRightEnd()
+    {
+        _aniComp.SetBool("rollRight", false);
+
+    }
+
+
+
 }
