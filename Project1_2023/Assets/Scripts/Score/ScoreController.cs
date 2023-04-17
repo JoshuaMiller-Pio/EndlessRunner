d4669af1d046b3e8a1df947ef228b376beb74275
WinnieThePio <81216801+JoshuaMiller-Pio@users.noreply.github.com>
2023-04-17 21:48:58 +0200

    added score on death

--- Project1_2023/Assets/Scripts/Score/ScoreController.cs
+++ Project1_2023/Assets/Scripts/Score/ScoreController.cs
@@ -34,7 +34,7 @@ public class ScoreController : MonoBehaviour
     {
         if (collision.gameObject.tag == "Obstacle")
         {
-            GameManager.Instance.gameoverScore(score);
+            GameManager.Instance.Playerscore = score;
         }
     }
     // Update is called once per frame
