8e2fadfe2f3fb8e6f09c100a22504128583481da
JordanZunde <jordanzunde@gmail.com>
2023-04-18 18:01:01 +0200

    Added final prefab

---
+++ Project1_2023/Assets/References.cs
@@ -0,0 +1,7 @@
+//Reference list:
+
+//2023_03_14_11_10__Game Development Lesson 5- sounds, Singleton class (2023) Vega School. Vega Scool. Available at: https://myvega.iielearn.ac.za/ultra/courses/_170695_1/cl/outline (Accessed: March 14, 2023).
+
+//Free low poly human - RPG character: Characters (2022) Unity Asset Store. Blink. Available at: https://assetstore.unity.com/packages/3d/characters/humanoids/fantasy/freelow-poly-human-rpg-character-219979 (Accessed: April 11, 2023). 
+
+//Iglesias, K. (2023) Basic motions free: 3D animations, Unity Asset Store. Kevin Iglesias.Available at: https://assetstore.unity.com/packages/3d/animations/basic-motions-free154271 (Accessed: April 11, 2023).
