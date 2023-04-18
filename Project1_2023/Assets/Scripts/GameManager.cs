6c7be868c530281b4f66e91e1a90a78a75ce4df5
JordanZunde <jordanzunde@gmail.com>
2023-04-18 19:06:04 +0200

    Fixed build error

--- Project1_2023/Assets/Scripts/GameManager.cs
+++ Project1_2023/Assets/Scripts/GameManager.cs
@@ -1,13 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.PackageManager;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.SocialPlatforms.Impl;
-using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager>
 {
