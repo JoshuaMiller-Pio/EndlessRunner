e95d539537191cf8125ce439c3e6957cc0d486b0
WinnieThePio <81216801+JoshuaMiller-Pio@users.noreply.github.com>
2023-05-26 14:07:33 +0200

    l

--- Project1_2023/Assets/Scripts/Boss1/LugiaAttack.cs
+++ Project1_2023/Assets/Scripts/Boss1/LugiaAttack.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class LugiaAttack : MonoBehaviour
 {
+    float time= 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,14 @@ public class LugiaAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            Fireball();
+        }
     }
+   void Fireball()
+   {
+
+   }
 }
