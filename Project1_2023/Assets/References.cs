24def95b300474e7ba7bb86ff980eb153a90e66a
WinnieThePio <81216801+JoshuaMiller-Pio@users.noreply.github.com>
2023-05-28 14:40:01 +0200

    Update References.cs

--- Project1_2023/Assets/References.cs
+++ Project1_2023/Assets/References.cs
@@ -4,6 +4,8 @@
 
 //Free low poly human - RPG character: Characters (2022) Unity Asset Store. Blink. Available at: https://assetstore.unity.com/packages/3d/characters/humanoids/fantasy/freelow-poly-human-rpg-character-219979 (Accessed: April 11, 2023). 
 
+//Hammarbäck, M. (no date) Fire sound efftect | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/fire-sound-efftect-21991/ (Accessed: 28 May 2023). 
+
 //Iglesias, K. (2023) Basic motions free: 3D animations, Unity Asset Store. Kevin Iglesias.Available at: https://assetstore.unity.com/packages/3d/animations/basic-motions-free154271 (Accessed: April 11, 2023).
 
 //Iglesias, K. (2023) Dance Animations FREE, Unity Asset store. Unity. Available at: https://assetstore.unity.com/packages/3d/animations/dance-animations-free-161313 (Accessed: 27 May 2023). 
@@ -12,9 +14,13 @@
 
 //Coma - Media(no date) Rock it | royalty-free music - pixabay, PixaBay. Available at: https://pixabay.com/music/upbeat-rock-it-21275/ (Accessed: 27 May 2023). 
 
+//Pixabay (no date) 9mm pistol shot | royalty free music - pixabay, Pixabay. Available at: https://pixabay.com/sound-effects/9mm-pistol-shot-6349/ (Accessed: 28 May 2023). 
+
 //pixabay(no date a) Blaster 2 | royalty-free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/blaster-2-81267/ (Accessed: 27 May 2023). 
 
 //pixabay(no date b) Droplets in a cave | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/droplets-in-a-cave-6785/ (Accessed: 27 May 2023). 
+
+//pixabay (no date) Fire spell | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/fire-spell-100276/ (Accessed: 28 May 2023). 
 
 //pixabay(no date c) Flapping | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/flapping-39306/ (Accessed: 27 May 2023). 
 
@@ -23,6 +29,8 @@
 //Pixabay(no date) Dungeon Air | Royalty Free Music - Pixabay, Pixabay. Available at: https://pixabay.com/sound-effects/dungeon-air-6983/ (Accessed: 27 May 2023). 
 
 //PixaBay(no date) Sci Fi Door | Royalty-free music - pixabay, PixaBay. Available at: https://pixabay.com/sound-effects/sci-fi-door-14782/ (Accessed: 27 May 2023). 
+
+//Textures.com (no date) FREE VOLCANIC ROCK CLIFF MATERIAL, Textures.com. Textures.com. Available at: https://www.textures.com/download/PBR0233/133289 (Accessed: 21 May 2023). 
 
 //Zen_man(no date) Background loop melodic techno #04 | royalty-free music - pixabay, PixaBay. Available at: https://pixabay.com/music/techno-trance-background-loop-melodic-techno-04-3822/ (Accessed: 27 May 2023). 
 
