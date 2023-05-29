7ff7f5bdad9f7c4c706f90a5414cdf03077c3ef7
JordanZunde <jordanzunde@gmail.com>
2023-05-29 13:12:18 +0200

    Update References.cs

--- Project1_2023/Assets/References.cs
+++ Project1_2023/Assets/References.cs
@@ -2,21 +2,32 @@
 
 //2023_03_14_11_10__Game Development Lesson 5- sounds, Singleton class (2023) Vega School. Vega Scool. Available at: https://myvega.iielearn.ac.za/ultra/courses/_170695_1/cl/outline (Accessed: March 14, 2023).
 
+
+//Textures:
+
 //3D SCANNED OLD LOG 4 (no date) Textures.com.Available at: https://www.textures.com/download/3DScans1013/142878 (Accessed: 28 May 2023). 
 
 //3D SCANNED TREE BARK  (no date) Textures.com. Available at: https://www.textures.com/download/3DScans0050/127280 (Accessed: 29 May 2023). 
 
-//Coma - Media(no date) Rock it | royalty-free music - pixabay, PixaBay. Available at: https://pixabay.com/music/upbeat-rock-it-21275/ (Accessed: 27 May 2023). 
+//PAINT RUSTED 3 PBR MATERIAL(no date) Textures.com.Available at: https://www.textures.com/download/PBR1068/142974 (Accessed: 28 May 2023). 
 
-//Free low poly human - RPG character: Characters (2022) Unity Asset Store. Blink. Available at: https://assetstore.unity.com/packages/3d/characters/humanoids/fantasy/freelow-poly-human-rpg-character-219979 (Accessed: April 11, 2023). 
+//Textures.com (no date) FREE VOLCANIC ROCK CLIFF MATERIAL, Textures.com. Textures.com. Available at: https://www.textures.com/download/PBR0233/133289 (Accessed: 21 May 2023). 
 
-//Hammarbäck, M. (no date) Fire sound efftect | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/fire-sound-efftect-21991/ (Accessed: 28 May 2023). 
+//Worn Bare Metal(no date) TexturesTextrures.com.Available at: https://www.textures.com/download/PBR1063/142895 (Accessed: 28 May 2023). 
+
+
+//Animations:
 
 //Iglesias, K. (2023) Basic motions free: 3D animations, Unity Asset Store. Kevin Iglesias.Available at: https://assetstore.unity.com/packages/3d/animations/basic-motions-free154271 (Accessed: April 11, 2023).
 
 //Iglesias, K. (2023) Dance Animations FREE, Unity Asset store. Unity. Available at: https://assetstore.unity.com/packages/3d/animations/dance-animations-free-161313 (Accessed: 27 May 2023). 
 
 //Magicpot Inc. (2016) Fighting Motions Vol.1, Unity Asset Store. Unity Asset Store. Available at: https://assetstore.unity.com/packages/3d/animations/fighting-motions-vol-1-76699 (Accessed: 27 May 2023). 
+
+
+//Blender Models:
+
+//Free low poly human - RPG character: Characters (2022) Unity Asset Store. Blink. Available at: https://assetstore.unity.com/packages/3d/characters/humanoids/fantasy/freelow-poly-human-rpg-character-219979 (Accessed: April 11, 2023).
 
 //Miller-Pio, J.(2023)."3D Model of 9mm bullet"[Blender]. Unpublished
 
@@ -32,7 +43,14 @@
 
 //Miller-Pio, J.(2023)."3D model of a Tunnel"[Blender]. Unpublished
 
-//PAINT RUSTED 3 PBR MATERIAL(no date) Textures.com.Available at: https://www.textures.com/download/PBR1068/142974 (Accessed: 28 May 2023). 
+//Zunde, J.(2021)."Lugia"[Blender]. Unpublished
+
+//Zunde, J.(2021)."Rock"[Blender]. Unpublished
+
+
+//Audio:
+
+//Coma - Media(no date) Rock it | royalty-free music - pixabay, PixaBay. Available at: https://pixabay.com/music/upbeat-rock-it-21275/ (Accessed: 27 May 2023). 
 
 //Pixabay (no date) 9mm pistol shot | royalty free music - pixabay, Pixabay. Available at: https://pixabay.com/sound-effects/9mm-pistol-shot-6349/ (Accessed: 28 May 2023). 
 
@@ -52,9 +70,7 @@
 
 //PixaBay(no date) Sci Fi Door | Royalty-free music - pixabay, PixaBay. Available at: https://pixabay.com/sound-effects/sci-fi-door-14782/ (Accessed: 27 May 2023). 
 
-//Textures.com (no date) FREE VOLCANIC ROCK CLIFF MATERIAL, Textures.com. Textures.com. Available at: https://www.textures.com/download/PBR0233/133289 (Accessed: 21 May 2023). 
-
-//Worn Bare Metal(no date) TexturesTextrures.com.Available at: https://www.textures.com/download/PBR1063/142895 (Accessed: 28 May 2023). 
+//Hammarbäck, M. (no date) Fire sound efftect | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/fire-sound-efftect-21991/ (Accessed: 28 May 2023). 
 
 //Zen_man(no date) Background loop melodic techno #04 | royalty-free music - pixabay, PixaBay. Available at: https://pixabay.com/music/techno-trance-background-loop-melodic-techno-04-3822/ (Accessed: 27 May 2023). 
 
