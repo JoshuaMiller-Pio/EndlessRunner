5660c808d5a858dff843db6cded60be7cb425b8e
JordanZunde <jordanzunde@gmail.com>
2023-05-29 13:17:47 +0200

    Update References.cs

--- Project1_2023/Assets/References.cs
+++ Project1_2023/Assets/References.cs
@@ -24,6 +24,8 @@
 
 //Magicpot Inc. (2016) Fighting Motions Vol.1, Unity Asset Store. Unity Asset Store. Available at: https://assetstore.unity.com/packages/3d/animations/fighting-motions-vol-1-76699 (Accessed: 27 May 2023). 
 
+//Zunde, J. (2021) "Lugia Idle Animation", Blender. Unpublished.
+
 
 //Blender Models:
 
@@ -52,25 +54,25 @@
 
 //Coma - Media(no date) Rock it | royalty-free music - pixabay, PixaBay. Available at: https://pixabay.com/music/upbeat-rock-it-21275/ (Accessed: 27 May 2023). 
 
+//Hammarbäck, M. (no date) Fire sound efftect | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/fire-sound-efftect-21991/ (Accessed: 28 May 2023). 
+
 //Pixabay (no date) 9mm pistol shot | royalty free music - pixabay, Pixabay. Available at: https://pixabay.com/sound-effects/9mm-pistol-shot-6349/ (Accessed: 28 May 2023). 
 
-//pixabay(no date a) Blaster 2 | royalty-free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/blaster-2-81267/ (Accessed: 27 May 2023). 
+//Pixabay(no date a) Blaster 2 | royalty-free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/blaster-2-81267/ (Accessed: 27 May 2023). 
 
-//pixabay(no date b) Droplets in a cave | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/droplets-in-a-cave-6785/ (Accessed: 27 May 2023). 
+//Pixabay(no date b) Droplets in a cave | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/droplets-in-a-cave-6785/ (Accessed: 27 May 2023). 
 
-//pixabay (no date) Fire spell | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/fire-spell-100276/ (Accessed: 28 May 2023). 
+//Pixabay (no date) Fire spell | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/fire-spell-100276/ (Accessed: 28 May 2023). 
 
-//pixabay(no date c) Flapping | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/flapping-39306/ (Accessed: 27 May 2023). 
+//Pixabay(no date c) Flapping | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/flapping-39306/ (Accessed: 27 May 2023). 
 
-//pixabay (no date) Free music 128bpm | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/free-music-128bpm-62830/ (Accessed: 28 May 2023). 
+//Pixabay (no date) Free music 128bpm | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/free-music-128bpm-62830/ (Accessed: 28 May 2023). 
 
-//pixabay(no date d) Running 1 | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/running-1-6846/ (Accessed: 27 May 2023). 
+//Pixabay(no date d) Running 1 | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/running-1-6846/ (Accessed: 27 May 2023). 
 
 //Pixabay(no date) Dungeon Air | Royalty Free Music - Pixabay, Pixabay. Available at: https://pixabay.com/sound-effects/dungeon-air-6983/ (Accessed: 27 May 2023). 
 
 //PixaBay(no date) Sci Fi Door | Royalty-free music - pixabay, PixaBay. Available at: https://pixabay.com/sound-effects/sci-fi-door-14782/ (Accessed: 27 May 2023). 
-
-//Hammarbäck, M. (no date) Fire sound efftect | royalty free music - pixabay, pixabay. Available at: https://pixabay.com/sound-effects/fire-sound-efftect-21991/ (Accessed: 28 May 2023). 
 
 //Zen_man(no date) Background loop melodic techno #04 | royalty-free music - pixabay, PixaBay. Available at: https://pixabay.com/music/techno-trance-background-loop-melodic-techno-04-3822/ (Accessed: 27 May 2023). 
 
