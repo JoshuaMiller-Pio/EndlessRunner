1f624135b093d1c1e498ff3351edf3658aced781
JordanZunde <jordanzunde@gmail.com>
2023-07-02 01:56:24 +0200

    TreeBoss death

--- Project1_2023/Assets/Scripts/Boss2/TreeController.cs
+++ Project1_2023/Assets/Scripts/Boss2/TreeController.cs
@@ -6,7 +6,7 @@ public class TreeController : Boss_Super
 {
     static Rigidbody RigComp;
     float timer = 2.5f;
-    static float health = 15;
+    static float health = 10;
 
     private void Start()
     {
